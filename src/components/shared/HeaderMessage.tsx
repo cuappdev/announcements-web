4e576f18cd79469f1bdeb1ea8bb7acd1db92d1f6
helenjb <bian.helenn@gmail.com>
2024-07-23 14:08:40 -0700

    WEB-6: Header message

---
+++ src/components/shared/HeaderMessage.tsx
@@ -0,0 +1,13 @@
+interface props {
+  heading: string;
+  subheading: string;
+}
+
+export default function HeaderMessage({ heading, subheading }: props) {
+  return (
+    <div className="flex flex-col items-start gap-2">
+      <h2 className="text-neutral-800">{heading}</h2>
+      <p className="b1 text-neutral-400">{subheading}</p>
+    </div>
+  );
+}
