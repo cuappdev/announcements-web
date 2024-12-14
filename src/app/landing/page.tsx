03490afa6ea9946c0484e08124bbae091d0e8472
Vin Bui <vdb23@cornell.edu>
2024-12-13 22:20:57 -0500

    Fix rendering issues

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,23 +1,10 @@
-"use client";
-
 import AuthGuard from "@/components/authGuard/authGuard";
 import Landing from "@/components/landing/landing";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function LandingPage() {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false, // default: true
-      },
-    },
-  });
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <AuthGuard>
-        <Landing />
-      </AuthGuard>
-    </QueryClientProvider>
+    <AuthGuard>
+      <Landing />
+    </AuthGuard>
   );
 }
